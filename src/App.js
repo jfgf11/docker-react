aa12000418d58af9663829561c5541d24045ea1d
Jorge Gaviria <jfgf11@gmail.com>
2023-05-03 11:35:09 -0500

    adding feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          rais....
+          Fui cambiado en feature
         </p>
         <a
           className="App-link"
